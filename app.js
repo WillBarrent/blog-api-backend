39a5d318288250f6703e8b024f2fa00cdf72fe77
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-08-16 16:48:24 +0500

    Add all necessary configs and routes to app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
+const express = require("express");
+const app = express();
+const auth = require("./routes/auth");
+const post = require("./routes/post");
+const comment = require("./routes/comment");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("./passport");
+
+app.use("/api/auth", auth);
+app.use("/api/posts", post);
+app.use("/api/comments", comment);
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log("App is listening on PORT", PORT);
+  console.log("App is listening on PORT", PORT);
 });
